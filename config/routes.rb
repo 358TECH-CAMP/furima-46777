925fc2e1fa4946bab9ac38b6a544574ffb852876
358TECH-CAMP <358hirata@gmail.com>
2026-01-10 23:46:32 +0900

    トップページエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # フリマアプリのトップページ
+  root "items#index"
+
+  # 商品リソース
+  resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
