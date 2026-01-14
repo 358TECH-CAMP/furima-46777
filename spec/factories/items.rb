a743b60984e91b551a6f8f97c08f6497acda84b9
358TECH-CAMP <358hirata@gmail.com>
2026-01-14 11:47:33 +0900

    テストエラーで変更

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,22 +1,17 @@
 FactoryBot.define do
   factory :item do
-    name                  { 'テスト商品' }
-    description           { 'テスト用の商品説明' } # info から description に修正
-    price                 { 300 }
-    category_id           { 2 }                # 1 から 2 に修正
-    status_id             { 2 }                # 1 から 2 に修正
-    delivery_fee_id       { 2 }                # 1 から 2 に修正
-    prefecture_id         { 2 }                # 1 から 2 に修正
-    scheduled_delivery_id { 2 }                # 1 から 2 に修正
-
+    name                   { 'テスト商品' }
+    info                   { 'テスト用の商品説明' } # descriptionから変更
+    category_id            { 2 }
+    sales_status_id        { 2 }                 # status_idから変更
+    shipping_fee_status_id { 2 }                 # delivery_fee_idから変更
+    prefecture_id          { 2 }
+    scheduled_delivery_id  { 2 }
+    price                  { 300 }
     association :user
 
     after(:build) do |item|
-      item.image.attach(
-        io: StringIO.new('test'),
-        filename: 'test.png',
-        content_type: 'image/png'
-      )
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
     end
   end
 end
