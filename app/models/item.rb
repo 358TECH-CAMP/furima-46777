750daba61e7776b51c56251f203b79348668ada9
358TECH-CAMP <358hirata@gmail.com>
2026-01-15 19:26:44 +0900

    商品購入機能:Itemモデルにorderとのアソシエーションを追加しNoMethodErrorを解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one :order # Itemモデルにorderとのアソシエーションを追加しNoMethodErrorを解消
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
