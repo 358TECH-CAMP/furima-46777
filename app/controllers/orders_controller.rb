6be068ca27910ee7a9a68e0ac03268a3087add60
358TECH-CAMP <358hirata@gmail.com>
2026-01-15 17:21:53 +0900

    商品購入機能：売却済みや出品者本人を弾く処理 と 決済を実行する処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user! # ログインしていない人はログイン画面へ
   before_action :set_item, only: [:index, :create]
+  # 出品者や売却済み商品の購入を防ぐ制限を追加
+  before_action :prevent_invalid_access, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -9,6 +11,8 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      # 保存の前に決済処理を実行する
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -27,5 +31,22 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  # PAY.JPに決済を依頼するメソッド
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  # 不正なアクセス（自分が出品した商品の購入など）をトップページへ戻す
+  def prevent_invalid_access
+    return unless current_user.id == @item.user_id || @item.order.present?
+
+    redirect_to root_path
   end
 end
