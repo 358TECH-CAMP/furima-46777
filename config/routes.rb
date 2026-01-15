be9686d6df4e86247f4cacb12fb9a1a3aa33ce93
358TECH-CAMP <358hirata@gmail.com>
2026-01-15 16:52:14 +0900

    購入機能のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  # editとupdateを追加　# destroyを追加
-    resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  # 既存の LGTM をもらった記述（only）を維持したまま、
+  # do ~ end を使って orders をネストさせます。
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :orders, only: [:index, :create]
+  end
 end
