8d37a07524e5d0c7678a31a5413ed7f98799e7b2
358TECH-CAMP <358hirata@gmail.com>
2026-01-18 14:24:25 +0900

    マイグレーションにremember_created_atカラムの存在チェックを追加

--- db/migrate/20260111090313_add_remember_created_at_to_users.rb
+++ db/migrate/20260111090313_add_remember_created_at_to_users.rb
@@ -1,5 +1,8 @@
 class AddRememberCreatedAtToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :remember_created_at, :datetime
-  end
+# usersテーブルに remember_created_at カラムがない場合のみ追加する
+  unless column_exists?(:users, :remember_created_at)    
+  add_column :users, :remember_created_at, :datetime
+   end
+ end
 end
