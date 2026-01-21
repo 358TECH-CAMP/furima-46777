3dfc8c9b252347c12ec224c879344d0a1ce03436
358TECH-CAMP <358hirata@gmail.com>
2026-01-21 22:14:25 +0900

    Capistranoのデプロイ対象ブランチをmainに設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :application, "furima-46777"
 # GitHubのリポジトリURL（SSH形式）
 set :repo_url, "git@github.com:358TECH-CAMP/furima-46777.git"
 
+set :branch, "main"
 # 共有ディレクトリの設定（デプロイ後も中身を維持するフォルダ）
 # これにより、デプロイのたびにログや画像が消えるのを防ぎます
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
