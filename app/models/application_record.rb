bb823d96d125d0f46c3e5bbeb2ec546b28a34749
358TECH-CAMP <358hirata@gmail.com>
2026-01-21 11:14:34 +0900

    MySQLキー長エラーの修正方法を変更

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  # 以下の1行を追加します
-  ActiveRecord::Schema.default_string_length = 191
+  # ここを以下のように書き換えてください
+  self.abstract_class = true
+  begin
+    ActiveRecord::Base.connection_config[:adapter] == 'mysql2' ? (ActiveRecord::Schema.default_string_length = 191) : nil
+  rescue StandardError
+    nil
+  end
 end
